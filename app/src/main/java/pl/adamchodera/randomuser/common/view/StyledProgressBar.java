b621546c9195b7846395db7d9cc9cca0c5ce2ad3
Adam Chodera <adam@chodera.com.pl>
2017-02-13 00:03:50 +0100

    Add missing constructors with styling

--- app/src/main/java/pl/adamchodera/randomuser/common/view/StyledProgressBar.java
+++ app/src/main/java/pl/adamchodera/randomuser/common/view/StyledProgressBar.java
@@ -1,5 +1,6 @@
 package pl.adamchodera.randomuser.common.view;
 
+
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.ProgressBar;
@@ -8,9 +9,21 @@ import pl.adamchodera.randomuser.R;
 
 public class StyledProgressBar extends ProgressBar {
 
+    public StyledProgressBar(final Context context) {
+        super(context);
+
+        setIntermediateStyle();
+    }
+
     public StyledProgressBar(final Context context, final AttributeSet attrs) {
         super(context, attrs);
 
+        setIntermediateStyle();
+    }
+
+    public StyledProgressBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        
         setIntermediateStyle();
     }
 
